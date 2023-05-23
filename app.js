b3e56b94c2bf2a9abd0f68d99d77803ec113470b
Jacob <jjgomez1989@yahoo.com>
2023-05-23 15:38:41 -0700

    expressTutorial

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-const express = require("express");
-const path = require("path");
-const app = express();
+const express = require('express')
+const app = express()
+const { products } = require('./data')
 
-//setup static and middleware
-app.use(express.static("./public"));
+app.get('/', (req, res) => {
+  res.json(products)
+})
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-// adding to static assets
-// SSR
-// });
-
-app.all("*", (req, res) => {
-  res.status(404).send("<h1>Page Not Found</h1>");
-});
-
-app.listen(5000, () => {
-  console.log("server is listening on port 5000...");
-});
+app.listen(4000, () => {
+  console.log('Server is listening on port 4000....')
+})
